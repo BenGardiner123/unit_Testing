900678066473985e29a7891ba9176f8d11b622e8
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-08-17 14:10:45 +1000

    adding testin librarys

---
+++ carLib/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace carLib
+{
+    public class Class1
+    {
+        public int Wheels;
+        public string Colour;
+        public string Rego;
+
+    }
+}
---
+++ personLib/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace personLib
+{
+    public class Person
+    {
+        
+    }
+}

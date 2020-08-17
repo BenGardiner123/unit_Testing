e0e55deaaed6be4d621111b1d11c31e5150343de
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-08-17 16:09:32 +1000

    to run go from projects folder - finsihed

---
+++ ConsoleProj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using personLib;
+
+namespace ConsoleProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            System.Console.WriteLine("Enter name");
+            string input = Console.ReadLine();
+            p.Name = input;
+
+            System.Console.WriteLine("Enter year of brth");
+            input = Console.ReadLine();
+            p.YearofBirth = int.Parse(input);
+
+            System.Console.WriteLine($"Hi {p.Name} you are {p.calcAge()} years old");
+        }
+    }
+}
